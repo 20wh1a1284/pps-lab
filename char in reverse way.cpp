77bc53050d22d84b57480b7e38105cc5e0cf6930
20wh1a1284 <87482556+20wh1a1284@users.noreply.github.com>
2021-07-21 12:10:03 +0530

    Add files via upload

---
+++ char in reverse way.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+ {
+    char char1 = 'P';
+    char char2 = 'P';
+    char char3 = 's';
+    
+    printf("The reverse of %c%c%c is %c%c%c\n",
+        char1, char2, char3,
+        char3, char2, char1);
+        
+   return(0);
+ }
